a13f09d9c017a0637e2caced7f5b48aa1c466165
Brodrigo04 <knightarmy350@gmail.com>
2024-03-11 22:09:16 -0400

    entire project

---
+++ README copy.md
@@ -0,0 +1,34 @@
+
+  # Table of Contents
+
+  * [Professional README Generator](#professional-readme-generator)
+  * [Description](#description)
+  * [Installation](#innodestallation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Questions](#questions)
+  
+  # tester
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/license/MIT)
+  
+  ## Description
+  this is a test
+  
+  ## Installation
+  npm i
+  
+  ## Usage 
+  in the terminal
+  
+
+  ## Contributing
+
+  dont
+  
+  ## Questions
+  
+  Questions? Contact me at [brandonlrodriguez25@gmail.com](mailto:brandonlrodriguez25@gmail.com)
+  
+  Link to GitHub profile for [brodrigo04](https://github.com/brodrigo04)
+  
+  
---
+++ index.js
@@ -0,0 +1,99 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Create questions 
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Project title?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Description?'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'License?',
+    choices: ['MIT', 'GPL', 'Apache']
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Describe install process:'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How is it used?'
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'GitHub profile:'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Your email address:'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contributing guidelines:'
+  }
+];
+
+// Call inquirer prompts
+inquirer.prompt(questions).then(answers => {
+
+  // Take user responses    
+  const title = answers.title;
+  const description = answers.description;
+
+  // Build README content
+  const readme =
+    `
+  # Table of Contents
+
+  * [Professional README Generator](#professional-readme-generator)
+  * [Description](#description)
+  * [Installation](#innodestallation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Questions](#questions)
+  
+  # ${answers.title}
+  [![License: ${answers.license}](https://img.shields.io/badge/License-${answers.license}-yellow.svg)](https://opensource.org/license/${answers.license})
+  
+  ## Description
+  ${answers.description}
+  
+  ## Installation
+  ${answers.installation}
+  
+  ## Usage 
+  ${answers.usage}
+  
+
+  ## Contributing
+
+  ${answers.contributing}
+  
+  ## Questions
+  
+  Questions? Contact me at [${answers.email}](mailto:${answers.email})
+  
+  Link to GitHub profile for [${answers.username}](https://github.com/${answers.username})
+  
+  `;
+
+  // Write to file
+  fs.writeFile('README.md', readme, (err) => {
+    if (err) throw err;
+    console.log('README created!');
+  });
+
+});
